33df25d9412cc9807064f79734a2239669fbe1d6
Missing-Tech <josephgrabski31@gmail.com>
2024-02-03 18:04:33 +0000

    Add lualine config

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,1 +1,15 @@
-return { "echasnovski/mini.surround", version = "*" }
+return {
+  "echasnovski/mini.surround",
+  version = "*",
+  opts = {
+    mappings = {
+      add = "gsa",
+      delete = "gsd",
+      find = "gsf",
+      find_left = "gsF",
+      highlight = "gsh",
+      replace = "gsr",
+      update_n_lines = "gsn",
+    },
+  },
+}
